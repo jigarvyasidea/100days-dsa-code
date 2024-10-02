989214f43f0342ad46f109f4a1ff2d2ce0204681
Jigar vyas <80967728+jigarvyasidea@users.noreply.github.com>
2024-10-02 21:56:39 +0530

    Create squarePattern.java

---
+++ squarePattern.java
@@ -0,0 +1,15 @@
+
+
+class Square {
+    public static void main(String[] args) {
+        int row = 4 ; 
+        int col = 6;
+        
+        for( int i = 1 ; i<row ; i++){
+            for ( int j = 1 ; j<col; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
